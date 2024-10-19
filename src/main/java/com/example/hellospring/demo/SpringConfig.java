835d17d9a5b60466a98ba81625bf2083ce7159d7
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-10-19 22:20:32 +0900

    순수 JDBC

--- src/main/java/com/example/hellospring/demo/SpringConfig.java
+++ src/main/java/com/example/hellospring/demo/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example.hellospring.demo;
 
+import com.example.hellospring.demo.repository.JdbcMemberRepository;
 import com.example.hellospring.demo.repository.MemberRepository;
-import com.example.hellospring.demo.repository.MemoryMemberRepository;
 import com.example.hellospring.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
